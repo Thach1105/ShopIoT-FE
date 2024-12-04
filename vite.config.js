46b9283e8ae879c6877dfdebeea7e3c87f2aeb30
thachnn <thach11052002@gmail.com>
2024-12-04 21:33:52 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
   plugins: [react()],
-  // build: {
-  //   outDir: "dist",
-  // },
+
   build: {
     rollupOptions: {
       external: ["react-quill/dist/quill.snow.css"],

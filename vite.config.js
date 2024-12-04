061ab71ce6adafa6b58aabb581bab4745cad719e
thachnn <thach11052002@gmail.com>
2024-12-04 21:43:13 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [vercel(), react()],
 
   build: {
     rollupOptions: {
